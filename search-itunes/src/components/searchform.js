88a2017db0e9cae6f547261a2d029e4c15d4083d
Shriram Shriram <shrshrir@publicisgroupe.net>
2019-02-20 12:36:06 +0530

    2.0

--- search-itunes/src/components/searchform.js
+++ search-itunes/src/components/searchform.js
@@ -4,9 +4,10 @@ export class SearchForm extends Component {
     constructor() { // properties go inside constructor
         super();
         this.state = {
-            name: "eminem",
-            age: 0,
-            user: {uname: "",age: 0},
+            name: "pi",
+            age: 31415,
+            gender: "other",
+            user: { name: "", age: 0, gender: "male" },
             users: []
         };
         this.update = this.update.bind(this);
@@ -16,21 +17,24 @@ export class SearchForm extends Component {
             <div>
                 <input name="name" value={this.state.name} onChange={this.update}></input>
                 <input name="age" value={this.state.age} onChange={this.update}></input>
+                <input type="radio" name="gender" value="male" onChange={this.update}/> Male
+                <input type="radio" name="gender" value="female" onChange={this.update}/> Female
+                <input type="radio" name="gender" value="other" onChange={this.update}/> Other
                 <button onClick={(event) => {
-                    this.state.user = {uname: this.state.name, age: this.state.age};
+                    this.state.user = { name: this.state.name, age: this.state.age, gender: this.state.gender};
                     this.state.users.push(this.state.user);
                     this.setState({
-                        users : this.state.users
+                        users: this.state.users
                     });
                 }}>Search</button>
                 <ol>
-                    { this.state.users.map((user) => <li>{user.uname + " " + user.age}</li>) }
+                    {this.state.users.map((user) => <li>{user.name + " " + user.age + " " + user.gender}</li>)}
                 </ol>
             </div>
         )
     }
 
-//////////////// not required since we are using event.target.name ///////////////////////
+    //////////////// not required since we are using event.target.name ///////////////////////
 
     // update_name(event) {
     //     this.setState({
@@ -44,7 +48,7 @@ export class SearchForm extends Component {
     //     });
     // }
 
-//////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////
 
     update(event) {
         this.setState({
